c1c7a9c5347ceeb9c7f79a04f86e32df7fdd7a65
Fábio Oliveira Pimentel <fabiopimentel.arte@gmail.com>
2021-03-22 10:18:55 -0300

    create simple APIREST

--- index.php
+++ index.php
@@ -4,3 +4,52 @@ ini_set( 'display_startup_errors', 1 );
 error_reporting( E_ALL );
 
 require __DIR__ . '/vendor/autoload.php';
+
+use App\Post;
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = explode( '/', $uri );
+$currency = ['BRL', 'USD', 'EUR'];
+
+
+if ($uri[1] !== 'exchange') {
+
+    header("HTTP/1.1 404 Not Found");
+    exit();
+}
+
+if ( !in_array($uri[2],$currency) ) {
+
+    header("HTTP/1.1 404 Not Found");
+    exit();
+}
+
+if ( !in_array($uri[3],$currency) ) {
+
+    header("HTTP/1.1 404 Not Found");
+    exit();
+}
+
+if (isset($uri[2])) {
+    $currencyFrom = (string) $uri[2];
+}
+
+if (isset($uri[3])) {
+    $currencyTo = (string) $uri[3];
+}
+
+$amount = filter_input(INPUT_GET, 'amount',FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+
+$price = filter_input(INPUT_GET, 'price',FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+// pass the request method and post ID to the Post and process the HTTP request:
+$controller = new Post($requestMethod, $currencyFrom, $currencyTo, $amount, $price);
+$controller->processRequest();
---
+++ src/Post.php
@@ -0,0 +1,58 @@
+<?php
+namespace App;
+
+class Post {
+  private $requestMethod;
+  private $currencyFrom;
+  private $currencyTo;
+  private $amount;
+  private $price;
+
+  public function __construct($requestMethod, $currencyFrom, $currencyTo, $amount, $price)
+  {
+    $this->requestMethod = $requestMethod;
+    $this->currencyFrom = $currencyFrom;
+    $this->currencyTo = $currencyTo;
+    $this->amount = $amount;
+    $this->price = $price;
+  }
+
+  public function processRequest()
+  {
+    switch ($this->requestMethod) {
+      case 'GET':
+        $response = $this->CurrencyConvert($this->currencyFrom, $this->currencyTo, $this->amount, $this->price);
+        break;
+      default:
+        $response = $this->notFoundResponse();
+        break;
+    }
+
+    //header($response['status_code_header']);
+    if ($response['body']) {
+        echo $response['body'];
+    }
+  }
+
+  public function CurrencyConvert(string $currencyFrom, string $currencyTo, float $amount, float $price)
+  {
+    $currency = [
+        'BRL' => 'R$', 
+        'USD' => '$', 
+        'EUR' => '€'
+    ];
+    $result = [
+        "convertedValue" => number_format($amount*$price, 2),
+        "symbol" => $currency[$currencyTo]
+    ];
+    $response['body'] = json_encode($result);
+    return $response;
+  }
+
+  private function notFoundResponse()
+  {
+    $response['status_code_header'] = 'HTTP/1.1 404 Not Found';
+    $response['body'] = null;
+    return $response;
+  }
+}
