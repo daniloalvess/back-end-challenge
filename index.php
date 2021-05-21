0c3feb965424ba47876e56744c62d557b3d5c341
Jordã França <jordan1992_16@hotmail.com>
2021-05-21 09:23:53 -0300

    Ajuste de nome nas pastas

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ ini_set( 'display_startup_errors', 1 );
 error_reporting( E_ALL );
 
 //Class
-require __DIR__.'/src/class/Validate.class.php';
-require __DIR__.'/src/class/Exchange.class.php';
+require __DIR__.'/src/Class/Validate.class.php';
+require __DIR__.'/src/Class/Exchange.class.php';
 
 //Imports
 require __DIR__ . '/vendor/autoload.php';
